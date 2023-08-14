fc9eaffbd661f9e2104d9dab35743a8416eb5f62
hello17 <mail4ltd@163.com>
2023-08-14 19:51:11 +0800

    feat: scss 文件的自动导入

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,9 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/element/index.scss" as *;`,
+        additionalData: `
+        @use "@/styles/element/index.scss" as *;
+        @use "@/styles/var.scss" as *;`,
       },
     },
   },
