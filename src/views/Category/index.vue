7a1c7f6d39379f79cefaca39d482957bcd5b7ca7
hello17 <mail4ltd@163.com>
2023-08-15 18:53:55 +0800

    feat: 一级分类 - 激活状态控制和分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import GoodsItem from '../Home/components/GoodsItem.vue'
 import { getTopCategoryAPI } from '@/apis/category'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -41,6 +42,26 @@ getBanner()
             <img :src="item.imgUrl" alt="" />
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <!-- 分类列表渲染 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }} -</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -15,7 +15,7 @@ const categoryStore = useCategoryStore()
       <!-- 导航区域 -->
       <ul class="app-header-nav">
         <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+          <RouterLink active-class="active" :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
       </ul>
 
