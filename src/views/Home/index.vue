6939a2a41beb477c742f9f397f4732e6008579b2
hello17 <mail4ltd@163.com>
2023-08-15 14:37:44 +0800

    feat: home - 面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,12 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物，好多商品">
+    <div>new</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐，好多商品">
+    <div>hot</div>
+  </HomePanel>
 </template>
