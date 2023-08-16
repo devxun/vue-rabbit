21c7b1a66d3065861ebf5a68375f7e8482ed4351
hello17 <mail4ltd@163.com>
2023-08-16 10:30:03 +0800

    feat: 二级分类 - 整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,16 +18,24 @@ const router = createRouter({
       children: [
         {
           path: '',
+          name: 'home',
           component: Home,
         },
         {
           path: '/category/:id',
+          name: 'category',
           component: Category,
+        },
+        {
+          path: 'category/sub/:id',
+          name: 'subCategory',
+          component: SubCategory,
         },
       ],
     },
     {
       path: '/login',
+      name: 'login',
       component: Login,
     },
   ],
