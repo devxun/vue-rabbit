ee1d85ca0c380e0b94fcaeb262085665b3188b0b
hello17 <mail4ltd@163.com>
2023-08-16 11:00:45 +0800

    feat: 二级分类 - 列表筛选功能实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -29,6 +29,12 @@ const getGoodList = async () => {
 getGoodList()
 
 import GoodsItem from '@/views/Home/components/GoodsItem.vue'
+
+// tab切换回调
+const tabChange = () => {
+  reqData.value.page = 1
+  getGoodList()
+}
 </script>
 
 <template>
@@ -42,7 +48,7 @@ import GoodsItem from '@/views/Home/components/GoodsItem.vue'
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
