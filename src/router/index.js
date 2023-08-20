8354b42f36400501444aee43a5c86a2e737d42b4
hello17 <mail4ltd@163.com>
2023-08-20 22:09:24 +0800

    feat: 购物车 - 购物车列表基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import Test from '@/views/Test/ParentComponent.vue'
+import CartList from '@/views/CartList/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,6 +38,11 @@ const router = createRouter({
           path: 'detail/:id',
           name: 'detail',
           component: Detail,
+        },
+        {
+          path: 'cartlist',
+          name: 'cartlist',
+          component: CartList,
         },
       ],
     },
