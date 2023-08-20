6839bfe3c6b1d5b0f919eec91ad9fb2e4092f1d1
hello17 <mail4ltd@163.com>
2023-08-20 20:56:11 +0800

    feat: 购物车 - 头部购物车列表渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -2,6 +2,8 @@
 // 使用 pinia 中的数据
 import { useCategoryStore } from '@/stores/category'
 const categoryStore = useCategoryStore()
+
+import CartHeader from './CartHeader.vue'
 </script>
 
 <template>
@@ -20,6 +22,7 @@ const categoryStore = useCategoryStore()
         <input type="text" placeholder="搜一搜" />
       </div>
       <!-- 头部购物车 -->
+      <CartHeader />
     </div>
   </header>
 </template>
