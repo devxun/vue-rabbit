e94f7b8e5afba54a5a6ee5833f6ab5630c0ce062
hello17 <mail4ltd@163.com>
2023-08-21 17:23:13 +0800

    feat: 支付 - 实现支付宝支付功能

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -10,6 +10,14 @@ const getPayInfo = async () => {
   payInfo.value = res.result
 }
 onMounted(() => getPayInfo())
+
+// 跳转地址
+// 携带订单 id 以及回跳地址跳转到支付地址（get）
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 </script>
 
 <template>
