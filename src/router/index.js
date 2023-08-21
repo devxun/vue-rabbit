7ec3ee4946bba2dbfde755b92fdfdabb76f10c51
hello17 <mail4ltd@163.com>
2023-08-21 17:34:29 +0800

    feat: 支付 - 支付结果展示

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Test from '@/views/Test/ParentComponent.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from '@/views/Pay/PayBack.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -55,6 +56,12 @@ const router = createRouter({
           path: 'pay',
           name: 'pay',
           component: Pay,
+        },
+        {
+          // http://localhost:5173/paycallback?payResult=true&orderId=1693555571879317506
+          path: 'paycallback', // 注意路径，必须是 paycallback
+          name: 'paycallback',
+          component: PayBack,
         },
       ],
     },
